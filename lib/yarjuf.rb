6494d234db3cfde8b6561ec0d58ee4177fbc2746
Nat Ritmeyer <nat@natontesting.com>
2012-11-10 11:33:46 +0000

    adding rspec formatter method signatures to JUnit

--- lib/yarjuf.rb
+++ lib/yarjuf.rb
@@ -3,6 +3,24 @@ require 'builder'
 require 'rspec/core/formatters/base_formatter'
 
 class JUnit < RSpec::Core::Formatters::BaseFormatter
+  def initialize(output)
+    super output
+  end
 
+  def example_passed(example)
+
+  end
+
+  def example_failed(example)
+
+  end
+
+  def example_pending(example)
+
+  end
+
+  def dump_summary(duration, example_count, failure_count, pending_count)
+
+  end
 end
 
