8a0601c32e1c99dee4a3dbaf952476deeabc6881
Nat Ritmeyer <nat@natontesting.com>
2012-11-13 14:56:08 +0000

    replacing `xpath` with `at_xpath`

--- features/step_definitions/suite_level_details_steps.rb
+++ features/step_definitions/suite_level_details_steps.rb
@@ -5,34 +5,34 @@ end
 
 Then /^the junit output reports one passing test$/ do
   step 'I parse the junit results file'
-  @results.xpath("/testsuites/@errors").first.value.should == "0"
-  @results.xpath("/testsuites/@failures").first.value.should == "0"
-  @results.xpath("/testsuites/@skipped").first.value.should == "0"
-  @results.xpath("/testsuites/@tests").first.value.should == "1"
+  @results.at_xpath("/testsuites/@errors").value.should == "0"
+  @results.at_xpath("/testsuites/@failures").value.should == "0"
+  @results.at_xpath("/testsuites/@skipped").value.should == "0"
+  @results.at_xpath("/testsuites/@tests").value.should == "1"
 end
 
 Then /^the junit output reports one failing test$/ do
   step 'I parse the junit results file'
-  @results.xpath("/testsuites/@errors").first.value.should == "0"
-  @results.xpath("/testsuites/@failures").first.value.should == "1"
-  @results.xpath("/testsuites/@skipped").first.value.should == "0"
-  @results.xpath("/testsuites/@tests").first.value.should == "1"
+  @results.at_xpath("/testsuites/@errors").value.should == "0"
+  @results.at_xpath("/testsuites/@failures").value.should == "1"
+  @results.at_xpath("/testsuites/@skipped").value.should == "0"
+  @results.at_xpath("/testsuites/@tests").value.should == "1"
 end
 
 Then /^the junit output reports one pending test$/ do
   step 'I parse the junit results file'
-  @results.xpath("/testsuites/@errors").first.value.should == "0"
-  @results.xpath("/testsuites/@failures").first.value.should == "0"
-  @results.xpath("/testsuites/@skipped").first.value.should == "1"
-  @results.xpath("/testsuites/@tests").first.value.should == "1"
+  @results.at_xpath("/testsuites/@errors").value.should == "0"
+  @results.at_xpath("/testsuites/@failures").value.should == "0"
+  @results.at_xpath("/testsuites/@skipped").value.should == "1"
+  @results.at_xpath("/testsuites/@tests").value.should == "1"
 end
 
 Then /^the junit output testsuite element contains a duration$/ do
   step 'I parse the junit results file'
-  @results.xpath("/testsuites/@time").first.value.should match /[.0-9]+/
+  @results.at_xpath("/testsuites/@time").value.should match /[.0-9]+/
 end
 
 Then /^the junit output testsuite element contains a timestamp$/ do
   step 'I parse the junit results file'
-  @results.xpath("/testsuites/@timestamp").first.value.should match /\d{4}-\d{2}-\d{2}T\d+:\d+:\d+\+\d+:\d+/
+  @results.at_xpath("/testsuites/@timestamp").value.should match /\d{4}-\d{2}-\d{2}T\d+:\d+:\d+\+\d+:\d+/
 end
