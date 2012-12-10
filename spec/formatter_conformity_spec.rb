96c1158872bde8007cb8376ed4cbbe3bd76dba4c
Nat Ritmeyer <nat@natontesting.com>
2012-12-10 14:49:05 +0000

    adding names to specs

--- spec/formatter_conformity_spec.rb
+++ spec/formatter_conformity_spec.rb
@@ -2,10 +2,21 @@ describe JUnit do
   context "interface conformity" do
     subject { JUnit.new "some output" }
 
-    it { should respond_to :example_passed }
-    it { should respond_to :example_failed }
-    it { should respond_to :example_pending }
-    it { should respond_to :dump_summary }
+    it "should respond to example passed" do
+      should respond_to :example_passed
+    end
+
+    it "should respond to example failed" do
+      should respond_to :example_failed
+    end
+
+    it "should respond to example pending" do
+      should respond_to :example_pending
+    end
+
+    it "should respond to dump summary" do
+      should respond_to :dump_summary
+    end
   end
 end
 
