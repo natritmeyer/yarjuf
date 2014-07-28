2f29c37e5ed065effe21c1b73bbc573253ec921d
Ben Snape <ben.snape@itv.com>
2014-07-28 11:23:01 +0100

    formatting and readibility changes

--- lib/yarjuf/j_unit.rb
+++ lib/yarjuf/j_unit.rb
@@ -39,7 +39,7 @@ class JUnit
     exception.nil? ? '' : "#{exception.message}\n#{formatted_backtrace}"
   end
 
-  #utility methods
+  # utility methods
 
   def self.count_in_suite_of_type(suite, test_case_result_type)
     suite.select { |example| example.metadata[:execution_result].status == test_case_result_type }.size
@@ -49,26 +49,37 @@ class JUnit
     example_notification.example.metadata[:example_group][:description]
   end
 
-  #methods to build the xml for test suites and individual tests
+  # methods to build the xml for test suites and individual tests
 
   def build_results(duration, example_count, failure_count, pending_count)
     @builder.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
-    @builder.testsuites :errors => 0, :failures => failure_count, :skipped => pending_count, :tests => example_count, :time => duration, :timestamp => Time.now.iso8601 do
+    @builder.testsuites(
+      :errors    => 0,
+      :failures  => failure_count,
+      :skipped   => pending_count,
+      :tests     => example_count,
+      :time      => duration,
+      :timestamp => Time.now.iso8601) do
       build_all_suites
     end
   end
 
   def build_all_suites
     @test_suite_results.each do |suite_name, tests|
-      build_test_suite suite_name, tests
+      build_test_suite(suite_name, tests)
     end
   end
 
   def build_test_suite(suite_name, tests)
-    failure_count = JUnit.count_in_suite_of_type tests, "failed"
-    skipped_count = JUnit.count_in_suite_of_type tests, "pending"
+    failure_count = JUnit.count_in_suite_of_type(tests, :failed)
+    skipped_count = JUnit.count_in_suite_of_type(tests, :pending)
 
-    @builder.testsuite :name => suite_name, :tests => tests.size, :errors => 0, :failures => failure_count, :skipped => skipped_count do
+    @builder.testsuite(
+      :name     => suite_name,
+      :tests    => tests.size,
+      :errors   => 0,
+      :failures => failure_count,
+      :skipped  => skipped_count) do
       @builder.properties
       build_all_tests tests
     end
@@ -85,10 +96,12 @@ class JUnit
     execution_time = test.metadata[:execution_result].run_time
     test_status    = test.metadata[:execution_result].status
 
-    @builder.testcase :name => test_name, :time => execution_time do
+    @builder.testcase(:name => test_name, :time => execution_time) do
       case test_status
-      when "pending" then @builder.skipped
-      when "failed" then build_failed_test test
+        when :pending
+          @builder.skipped
+        when :failed
+          build_failed_test test
       end
     end
   end
@@ -96,9 +109,9 @@ class JUnit
   def build_failed_test(test)
     failure_message = "failed #{test.metadata[:full_description]}"
 
-    @builder.failure :message => failure_message, :type => "failed" do
-      @builder.cdata! failure_details_for test
+    @builder.failure(:message => failure_message, :type => 'failed') do
+      @builder.cdata!(failure_details_for(test))
     end
   end
-end
 
+end
